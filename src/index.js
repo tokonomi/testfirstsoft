05df51df630b6bfa15b42791057881bcf7e8bf00
Walid <foowie99@gmail.com>
2023-01-30 17:33:15 +0400

    inital project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 
 createRoot(document.querySelector("#root")).render(
